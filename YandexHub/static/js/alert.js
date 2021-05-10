467e88a077b531855cc2f2bd14fd73da5d4ecb4b
TheProgersTeam <68988841+ValeryChapman@users.noreply.github.com>
2021-05-10 12:04:59 +0300

    nc

---
+++ YandexHub/static/js/alert.js
@@ -0,0 +1,14 @@
+function view_alert(text) {
+    document.querySelectorAll('.toast').forEach((elem) => {
+        elem.style.display = 'none';
+        elem.style.display = 'block';
+        elem.className = "toast show";
+    });
+    document.getElementById('toast-body').innerHTML = text;
+
+    document.querySelectorAll('.toast').forEach((elem) => {
+        setTimeout(function () {
+            elem.style.display = 'none';
+        }, 7500);
+    });
+}
