ad2856f6d78c1e5a4e009eba55d0ee125299f869
TheProgersTeam <68988841+ValeryChapman@users.noreply.github.com>
2021-05-26 23:57:00 +0300

    nc

--- YandexHub/mainapp/static/js/search/main.js
+++
@@ -1,9 +0,0 @@
-async function search() {
-    var input = document.getElementById("search-input");
-    var button = document.getElementById("search-button");
-    if (String(input.value).length != 0) {
-        //window.location = `${location.protocol}//${location.hostname + (location.port ? ':' + location.port : '')}/results/search_query=${input.value}`;
-        transition_link(`${location.protocol}//${location.hostname + (location.port ? ':' + location.port : '')}/results/search_query=${input.value}`);
-        
-    }
-}
